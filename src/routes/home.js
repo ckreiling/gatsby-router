7d02143bb0a9aac93c9ce0d74fe25bce29c87264
Christian Kreiling <christian.kreiling@gmail.com>
2018-10-22 11:10:28 -0400

    Fixed up code

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,12 +3,14 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
+import { paths } from './'
+
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to={paths.page2}>Go to page 2</Link>
   </Layout>
 )
 
--- src/routes/page-2.js
+++ src/routes/page-2.js
@@ -3,11 +3,13 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
+import { paths } from './'
+
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to={paths.home}>Go back to the homepage</Link>
   </Layout>
 )
 
